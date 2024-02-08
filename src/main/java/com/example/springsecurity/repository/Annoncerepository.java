5113ba7684e6a25e8b59af48018ae732834ef220
Kanty <119051551+KantyRas@users.noreply.github.com>
2024-02-08 23:23:28 +0300

    huuh

--- src/main/java/com/example/springsecurity/repository/Annoncerepository.java
+++ src/main/java/com/example/springsecurity/repository/Annoncerepository.java
@@ -16,9 +16,8 @@ public interface Annoncerepository extends JpaRepository<Annonce,Integer> {
     @Query("select a from Annonce a where a.validation = :validation")
     List<Annonce> validation(int validation);
 
-    @Query("SELECT a FROM Annonce a WHERE a.prix BETWEEN :minPrix AND :maxPrix or a.idmarque= :idmarque  or a.modele=:modele or a.idcategorie= :idcategorie")
-    List<Annonce> recherche(double minPrix,double maxPrix,int idmarque,String modele,int idcategorie);
-
+    @Query("SELECT DISTINCT a FROM Annonce a JOIN a.categories ac JOIN a.marque am WHERE a.prix BETWEEN :minPrix AND :maxPrix OR am.idmarque = :idmarque OR a.modele = :modele OR ac.idCategorie = :idcategorie")
+    List<Annonce> recherche(double minPrix, double maxPrix, int idmarque, String modele, int idcategorie);
     @Query("Select a From Annonce a where a.idannonce= :idannonce")
     List<Annonce> findbyidannonce(int idannonce);
 }
